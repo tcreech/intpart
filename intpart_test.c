fd76da703030cf07e6feeb545cdbcde58812d8eb
Tim Creech <tcreech@umd.edu>
2014-06-06 15:56:06 -0400

    update driver to accept floats as args

--- intpart_test.c
+++ intpart_test.c
@@ -1,9 +1,25 @@
 #include <intpart.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
-   float floatpart[3] = {1/3.0 + 1/20.0, 1/2.0 - 1/10.0, 1/6.0 + 1/20.0};
-   int intpart[3];
+int main(int argc, char** argv){
+   int l = argc-1;
+   if(l <= 0){
+      printf("Need some floats!\n");
+      return 1;
+   }
+
+   float* floatpart;
+   int* intpart;
+
+   floatpart = malloc(sizeof(float) * l);
+   intpart = malloc(sizeof(int) * l);
+
+   int argi;
+   for(argi=1; argi<argc; argi++){
+      floatpart[argi-1] = atof(argv[argi]);
+   }
+
    int n = 240;
    int chunksize = 4;
 
@@ -16,29 +32,31 @@ int main(void){
    printf("chunksize = %d\n", chunksize);
    printf("\n");
 
-   intpart_from_floatpart_chunked(n, intpart, floatpart, chunksize, 3);
+   intpart_from_floatpart_chunked(n, intpart, floatpart, chunksize, l);
 
    int sum = 0;
    int i;
-   for(i=0; i<3; i++){ sum += intpart[i]; }
-   printf("intpart = {%d, %d, %d}; sum = %d\n",
-         intpart[0],
-         intpart[1],
-         intpart[2],
-         sum
-         );
+   for(i=0; i<l; i++){ sum += intpart[i]; }
+   printf("intpart = {");
+   for(i=0; i<l; i++){
+      printf("%d",intpart[i]);
+      if(i<l-1)
+         printf(", ");
+   }
+   printf("}; sum = %d\n\n", sum);
 
    printf("equipartitioning gives:\n");
-   intpart_equipartition_chunked(n, intpart, chunksize, 3);
+   intpart_equipartition_chunked(n, intpart, chunksize, l);
 
    sum = 0;
-   for(i=0; i<3; i++){ sum += intpart[i]; }
-   printf("intpart = {%d, %d, %d}; sum = %d\n",
-         intpart[0],
-         intpart[1],
-         intpart[2],
-         sum
-         );
+   for(i=0; i<l; i++){ sum += intpart[i]; }
+   printf("intpart = {");
+   for(i=0; i<l; i++){
+      printf("%d",intpart[i]);
+      if(i<l-1)
+         printf(", ");
+   }
+   printf("}; sum = %d\n", sum);
 
    return 0;
 }
